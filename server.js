e48563a5c3a927263f610de5f74aed0be5582250
smani01 <sreedivyanambiar@gmail.com>
2016-09-27 00:47:08 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/Article-One', function (req, res) {
-  res.send('Article One requested and will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'Article-One.html'));
 });    
 
 app.get('/Article-Two', function (req, res) {
