cb6db9175f9d315fb2fb6f5040644ac6c61e2fd3
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-09-29 00:34:36 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element =  document.getElementById('main-text');
 element.innerHTML="New Value";
+
+var img =  document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft = 100px
+}
