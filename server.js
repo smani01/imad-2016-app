b59809d5e8271019b8bfa759e0dc71fd0e509b28
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-09-28 16:50:19 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ var articles = {
     }
 };
 
-function createTemplate (data) {
+function createTemplate(data) {
     var title = data.title;
     var date  = data.date;
     var heading = data.heading;
