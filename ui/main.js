43e734d1c12d370ae16efb26a1be51d2153113d8
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-09-30 01:17:14 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,8 @@ button.onclick = function() {
         if (request.readystate === XMLHttpRequest.DONE){
             //Take some action
             if (request.status === 200){
-                var counter = request.responseText;
+           //     var counter = request.responseText;
+           counter=counter+1;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
             }
