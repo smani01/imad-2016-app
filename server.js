cf8f361b297db0adefc4f09c4248bdc9c62e0e9d
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-10-01 00:10:04 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,9 +116,10 @@ app.get('submit-name/:name',function (req, res) {
     names.push(name);
     //JSON - Javascript Object Notation
     
-    res.send(JSON.stringify(names); 
+    res.send(JSON.stringify(names)); 
     
-}
+});
+
 app.get('/:articleName', function (req, res) {
     //articlename=article-one
     //articles[articleName]={} content for the article one
