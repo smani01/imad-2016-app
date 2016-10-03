6f2ea1deed8e3ebaec7c30209b0a72a9e5de8213
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-10-04 03:20:09 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get('/counter', function (req, res) {
     res.send(counter.toString());
 });
 
-var names[];
+var names = [];
 app.get('submit-name/:name',function (req, res) {
     // Get the name from the request
     var name=req.params.name; // 1000
