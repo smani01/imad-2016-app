3b6b8408e4c7fad6401b6b563a8f2724c7d802ce
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-10-04 03:23:25 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 var names = [];
-app.get('submit-name/:name',function (req, res) {
+app.get('/submit-name/:name',function (req, res) {
     // Get the name from the request
     var name=req.params.name; // 1000
     
