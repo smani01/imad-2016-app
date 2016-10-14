0362aa127f4b02ad3117cf35d0aac0b551d3c201
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-10-15 03:05:35 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -46,6 +46,10 @@ var articles = {
             <p>
                 I am hoping to make this an interactive place where readers can come to share their thoughts on my stories. You can let me know what you liked, what you did not, or what you might be interested in seeing here in future.
                 Thank You for swinging by my blog. And pop back again soon. Bye !!
+                <br>
+                <br>
+                Cheers..
+                Sreedivya
             </p>`
 },
     Contact: {
