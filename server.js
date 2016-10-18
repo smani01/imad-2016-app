85611e787e2115375a458a0a46550ebd1253238c
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-10-19 00:27:06 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var articles = {
                          This is the content of my first article.This is the content of my first article.This is the content of my first article.
                          This is the content of my first article.This is the content of my first article.
                     </p>`,
-                    Usercomments:'Like <Button Id="counter">ClickMe!</Button> :- <span id="count">0</span> times'
+                    Usercomments:'<Button Id="counter">Like</Button> :- <span id="count">0</span> times'
 },
     About: {
         title: 'About | Sreedivya',
@@ -52,7 +52,7 @@ var articles = {
                 Cheers..
                 Sreedivya
             </p>`,
-            Usercomments:'Like <Button Id="counter">ClickMe!</Button> :- <span id="count">0</span> times'
+            Usercomments:'<Button Id="counter">Like</Button> :- <span id="count">0</span> times'
 },
     Contact: {
         title: 'Contact | Sreedivya',
@@ -72,7 +72,7 @@ var articles = {
             <li><a href="https://twitter.com/smani01/" target="_blank"> <img src ="http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,h_200,w_200/svvszaufolhr4jkwl7vz.jpg" sizes=auto> </a></li>
         </div> 
             </p>`,
-            Usercomments:'Like <Button Id="counter">ClickMe!</Button> :- <span id="count">0</span> times'
+            Usercomments:'<Button Id="counter">Like</Button> :- <span id="count">0</span> times'
     }
 };
 
