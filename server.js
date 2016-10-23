5c0a5baa6e9bd66fc31b0e6399bf9548d211df0f
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-10-24 01:39:15 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -159,20 +159,12 @@ app.get('/submit_comment',function(req,res){
     //to get the comments
  var comment=req.query.comment;
  comments.push(comment);
- console.log('comments is: ',comments);
  res.send(JSON.stringify(comments));
 
     //to render those comments on the page
 });
 
-app.get('/fetchcomments', function(req, res) {
-  var comment = req.query.comment;
-	if (comments !== undefined)
-	  res.send(JSON.stringify(comments));
-	else {
-	  res.send("null");
-	}
-});
+
 
 
 app.get('/:articleName', function (req, res) {
