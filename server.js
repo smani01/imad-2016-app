4ca5b1a1c264202201aa745b48529fee82c0fd76
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-10-24 00:10:12 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ var articles = {
             <br>
              
            <input type="text" id="Comments" placeholder="Enter your comments.." >
-           <input type="submit" value="enter" id="submit_cmnt">
+           <input type="submit" value="Submit" id="submit_cmnt">
            <ul1 id="commentlist">
            </ul1>
            </div>
