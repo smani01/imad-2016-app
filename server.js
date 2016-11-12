eff3629f4418518650574f564d0073ba2bf6750c
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-11-13 00:26:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ app.get('/articles/:articleName', function (req, res) {
     //articles[articleName]={} content for the article one
     var articleName = req.params.articleName;
     
-    pool.query("SELECT * FROM article where title = " + req.params.articleName, function (err, result) {
+    pool.query("SELECT * FROM article where title = '" + req.params.articleName + "'", function (err, result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
