7d3fdbd6e33936a5263b3722ab6fcf81a5801277
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-11-12 15:27:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'smani01',
+    database:'smani01',
+    host:'http://db.imad.hasura-app.io/',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -131,10 +140,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
     //make a select request
     //return response with the results
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ pool.query('SELECT * from article',function(err,result){
+     if (err){
+     res.status(500).send(err.tostring());
+     }
+     else{
+     res.send(JSON.Stringify(result.rows));
+     }
+ });
 });
 
 
