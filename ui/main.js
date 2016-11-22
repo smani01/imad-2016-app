af79644907079a9d6cedcd7d36d756fc93c27b7b
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-11-23 00:24:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ class="close" title="Close Modal">&times;</span>
       <label><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="psw" required>
 
-      <button type="submit" style="width:100px">Login</button>
+      <button type="submit">Login</button>
       <input type="checkbox" checked="checked"> Remember me
     </div>
 
