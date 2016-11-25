49c35ec1fa8b5f1482a8fd4b62e67aa3fd65c8fe
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-11-26 01:23:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -219,7 +219,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
 
 app.get('/articles/:articleName', function (req, res) {
   // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
-  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
+  pool.query("SELECT * FROM article WHERE title = $1 and id not in (1,2,3)", [req.params.articleName], function (err, result) {
     if (err) {
         res.status(500).send(err.toString());
     } else {
