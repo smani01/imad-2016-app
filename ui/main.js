3b6760b7c44d5f0802ed9d50213f7f45ccd38e91
Sreedivya N M <sreedivyanambiar@gmail.com>
2016-11-29 01:06:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 function loadLoginForm () {
     var loginHtml = `
-       <a href="#" data-toggle="modal" data-target="#login-modal">Login</a>
+       <a href="#" data-toggle="modal" data-target="#login-modal">Login</a>;
 <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
     	  <div class="modal-dialog">
 				<div class="loginmodal-container">
